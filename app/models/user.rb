d3d0146e9be3f4f72ee465bfb329d11b6acf2b61
ikeda kazuki <kazuki.ikeda@kachiri.co.jp>
2018-12-26 15:54:41 +0900

    feature/add Search model of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :Order
+
+  def self.search(search)
+    if search
+     User.where(['name Like ?',"%#{search}%"])
+    else
+     User.all
+    end
+  end
 end
